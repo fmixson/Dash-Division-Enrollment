47f15146ac6f3c79421b521fe802e56f1755984f
fmixson <flmixson@cerritos.edu>
2022-12-20 14:51:06 -0800

    12/20/22 Commit

---
+++ division_dashboard.py
@@ -0,0 +1,140 @@
+from dash import Dash, dash_table, html, dcc, Input, Output
+import pandas as pd
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy
+
+df = pd.read_csv('C:/Users/fmixson/PycharmProjects/DivisionEnrollment/Spring_Division_Enrollment.csv', encoding='Latin')
+
+app = Dash(__name__,
+           external_stylesheets=[dbc.themes.BOOTSTRAP]
+           )
+app.layout = html.Div(
+    [
+    html.Div(
+    dcc.Dropdown(id='dept_dropdown',
+                 options=[{'label': dept, 'value': dept}
+                          for dept in df['Dept'].unique()]
+                 )),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='pie_chart'), width=6),
+        dbc.Col(dcc.Graph(id='session_pie_chart'), width=6)
+            ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='modality_barchart'), width=6),
+        dbc.Col(dcc.Graph(id='session_barchart'), width=6)
+    ])
+
+    ])
+
+@app.callback(
+    Output(component_id='modality_barchart', component_property='figure'),
+    Input(component_id='dept_dropdown', component_property='value')
+)
+def modality_barchart(dept):
+    if dept is None:
+        subset_df = df[['Dept', 'Room', 'Class', 'Size', 'Max']]
+        group_modalities = subset_df.groupby('Room').agg({'Class':'count', 'Size': 'sum', 'Max': 'sum'}).reset_index()
+        group_modalities['Fill'] = group_modalities['Size'] / group_modalities['Max']
+        group_modalities = group_modalities.iloc[1:5, :]
+
+        bar_fig = go.Figure(data=[
+            go.Bar(name='Size', x=group_modalities['Room'], y=group_modalities['Size']),
+            go.Bar(name='Capacity', x=group_modalities['Room'],y=group_modalities['Max'])
+        ])
+        bar_fig.update_layout(barmode='group')
+        return bar_fig
+    else:
+        subset_df = df[['Dept', 'Room', 'Class', 'Size', 'Max']]
+        subset_dff = subset_df[subset_df['Dept'] == dept]
+        group_modalities = subset_dff.groupby('Room').agg({'Class': 'count', 'Size': 'sum', 'Max': 'sum'}).reset_index()
+        group_modalities['Fill'] = group_modalities['Size'] / group_modalities['Max']
+        # group_modalities = group_modalities.iloc[1:5, :]
+
+        bar_fig = go.Figure(data=[
+            go.Bar(name='Size', x=group_modalities['Room'], y=group_modalities['Size']),
+            go.Bar(name='Capacity', x=group_modalities['Room'], y=group_modalities['Max'])
+        ])
+        bar_fig.update_layout(barmode='group')
+
+        return bar_fig
+
+@app.callback(
+    Output(component_id='pie_chart', component_property='figure'),
+    Input(component_id='dept_dropdown', component_property='value')
+)
+def modality_pie_chart(dept):
+    if dept is None:
+        subset_df = df[['Dept', 'Room', 'Class', 'Size', 'Max']]
+        group_modalities = subset_df.groupby('Room').agg({'Class':'count', 'Size': 'sum', 'Max': 'sum'}).reset_index()
+        total_count = group_modalities['Class'].sum()
+        group_modalities['Perc'] = group_modalities['Class'] / total_count
+        fig = px.pie(group_modalities, values='Perc', names='Room')
+        return fig
+    else:
+        subset_df = df[['Dept', 'Room', 'Class', 'Size', 'Max']]
+        subset_dff = subset_df[subset_df['Dept'] == dept]
+        group_modalities = subset_dff.groupby('Room').agg({'Class': 'count', 'Size': 'sum', 'Max': 'sum'}).reset_index()
+        total_count = group_modalities['Class'].sum()
+        group_modalities['Perc'] = group_modalities['Class'] / total_count
+        fig = px.pie(group_modalities, values='Perc', names='Room')
+        return fig
+
+@app.callback(
+    Output(component_id='session_pie_chart', component_property='figure'),
+    Input(component_id='dept_dropdown', component_property='value')
+)
+def session_pie_chart(dept):
+    if dept is None:
+        subset_df = df[['Dept', 'Session', 'Class', 'Size', 'Max']]
+        group_modalities = subset_df.groupby('Session').agg({'Class':'count', 'Size': 'sum', 'Max': 'sum'}).reset_index()
+        total_count = group_modalities['Class'].sum()
+        group_modalities['Perc'] = group_modalities['Class'] / total_count
+        session_fig = px.pie(group_modalities, values='Perc', names='Session')
+        return session_fig
+    else:
+        subset_df = df[['Dept', 'Session', 'Class', 'Size', 'Max']]
+        subset_dff = subset_df[subset_df['Dept'] == dept]
+        group_modalities = subset_dff.groupby('Session').agg({'Class': 'count', 'Size': 'sum', 'Max': 'sum'}).reset_index()
+        total_count = group_modalities['Class'].sum()
+        group_modalities['Perc'] = group_modalities['Class'] / total_count
+        session_fig = px.pie(group_modalities, values='Perc', names='Session')
+        return session_fig
+
+@app.callback(
+        Output(component_id='session_barchart', component_property='figure'),
+        Input(component_id='dept_dropdown', component_property='value')
+    )
+def session_barchart(dept):
+        if dept is None:
+            subset_df = df[['Dept', 'Session', 'Class', 'Size', 'Max']]
+            group_sessions = subset_df.groupby('Session').agg(
+                {'Class': 'count', 'Size': 'sum', 'Max': 'sum'}).reset_index()
+            group_sessions['Fill'] = group_sessions['Size'] / group_sessions['Max']
+            # group_sessions = group_sessions.iloc[1:5, :]
+            print(group_sessions)
+            bar_fig = go.Figure(data=[
+                go.Bar(name='Size', x=group_sessions['Session'], y=group_sessions['Size']),
+                go.Bar(name='Capacity', x=group_sessions['Session'], y=group_sessions['Max'])
+            ])
+            bar_fig.update_layout(barmode='group')
+            return bar_fig
+        else:
+            subset_df = df[['Dept', 'Session', 'Class', 'Size', 'Max']]
+            subset_dff = subset_df[subset_df['Dept'] == dept]
+            group_sessions = subset_dff.groupby('Session').agg(
+                {'Class': 'count', 'Size': 'sum', 'Max': 'sum'}).reset_index()
+            group_sessions['Fill'] = group_sessions['Size'] / group_sessions['Max']
+            # group_sessions = group_sessions.iloc[1:5, :]
+            print(group_sessions)
+            bar_fig = go.Figure(data=[
+                go.Bar(name='Size', x=group_sessions['Session'], y=group_sessions['Size']),
+                go.Bar(name='Capacity', x=group_sessions['Session'], y=group_sessions['Max'])
+            ])
+            bar_fig.update_layout(barmode='group')
+
+            return bar_fig
+
+
+app.run(debug=True)
